f15bc272b8f868af6fef9caafa9321907373537d
yusufsrmli <yusufsurmeli@gmail.com>
2020-08-06 01:47:51 +0300

    Deneme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
